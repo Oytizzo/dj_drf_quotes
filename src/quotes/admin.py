cfbb0ce87644316e812fa235b8125191308f30d1
Oytizzo <oytizzo@gmail.com>
2021-04-24 10:47:27 +0600

    Admin implimented and update model

--- src/quotes/admin.py
+++ src/quotes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Quote, QuoteTag, Author
+
+admin.site.register(Quote)
+admin.site.register(QuoteTag)
+admin.site.register(Author)
--- src/quotes/models.py
+++ src/quotes/models.py
@@ -5,12 +5,21 @@ class Author(models.Model):
     name = models.CharField(max_length=256)
     about = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 
 class QuoteTag(models.Model):
     tag_name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return  self.tag_name
+
 
 class Quote(models.Model):
     quote = models.TextField()
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
     quote_tag = models.ManyToManyField(QuoteTag)
+
+    def __str__(self):
+        return self.quote
