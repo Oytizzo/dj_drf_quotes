82bc856d4947b407b4cc0d4cf02e20e4ed6ac7d6
Oytizzo <oytizzo@gmail.com>
2021-04-28 21:43:37 +0600

    Modified quote models.py

--- src/quotes/models.py
+++ src/quotes/models.py
@@ -13,7 +13,7 @@ class QuoteTag(models.Model):
     tag_name = models.CharField(max_length=50)
 
     def __str__(self):
-        return  self.tag_name
+        return self.tag_name
 
 
 class Quote(models.Model):
