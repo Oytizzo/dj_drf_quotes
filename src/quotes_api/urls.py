943f9fb76abfe4119e88ac976cc4ad91e938626b
Oytizzo <oytizzo@gmail.com>
2021-04-30 21:25:44 +0600

    Added urls for detail and new quote

--- src/quotes_api/urls.py
+++ src/quotes_api/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from .views import QuoteApiView, AuthorApiView, QuoteTagApiView
 
 urlpatterns = [
-    path('', QuoteApiView.as_view(), name='quote_api'),
+    path('quotes/', QuoteApiView.as_view(), name='quote_api'),
     path('author/', AuthorApiView.as_view(), name='author_api'),
     path('quote_tag/', QuoteTagApiView.as_view(), name='quote_tag_api'),
+    path('quotes/<int:pk>/', QuoteApiDetailView.as_view(), name='quote_api_detail'),
+    path('quotes/new/', QuoteApiNewView.as_view(), name='quote_api_new'),
 ]
